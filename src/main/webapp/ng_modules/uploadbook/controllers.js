d5961e87d02c1168ca3c0785c473ef887fc8b461
fususenlin <287300953@qq.com>
2014-06-26 18:12:50 +0800

    慢慢来

--- src/main/webapp/ng_modules/uploadbook/controllers.js
+++ src/main/webapp/ng_modules/uploadbook/controllers.js
@@ -4,13 +4,13 @@ var UploadbookCtrl = function($scope,$fileUploader) {
 			 browseLabel:"浏览",
 			 uploadLabel:"上传",
 			 removeLabel:"移除"
-	       // initialPreview: ["<img src='assets.jpg' class='file-preview-image'>", "<img src='Jellyfish.jpg' class='file-preview-image'>"]
 	    });
-	    $("#file-3").fileinput({
-	        showCaption: false,
-	        browseClass: "btn btn-primary btn-lg",
-	        fileType: "any"
+		$("#file-2").fileinput({
+			 browseLabel:"浏览",
+			 uploadLabel:"上传",
+			 removeLabel:"移除"
 	    });
+	   
 	    $(".btn-warning").on('click', function() {
 	        $("#file-4").attr('disabled', 'disabled');
 	        $('#file-4').fileinput('refresh', {browseLabel: 'Kartik'});
